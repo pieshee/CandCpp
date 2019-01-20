e855e08203d0981feb4e5cd1bed80ccab35670bb
PouriaBakhshi <39597756+pouriabakhshi@users.noreply.github.com>
2019-01-20 11:56:26 +0330

    Update problem1.c
    edit loop

--- Fundamental-Clang/problem1.c
+++ Fundamental-Clang/problem1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 void main(){
-//loop
-printf("hello");
+//simple loop
+//for / while/ do while /
+//for(initializationStatement; testExpression; updateStatement) {write code}
+int i;
+for(i=0; i<5;i++){
+printf("value of i:%d\n",i);
+}
+return 0;
 }

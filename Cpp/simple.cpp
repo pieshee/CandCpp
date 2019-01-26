b0aebdd8dc05a77b27af616f98f25c5229c5d5cd
PouriaBakhshi <39597756+pouriabakhshi@users.noreply.github.com>
2019-01-26 21:17:47 +0330

    Update simple.cpp
    edit

--- Cpp/simple.cpp
+++ Cpp/simple.cpp
@@ -1,5 +1,9 @@
-include<iostream>
+// a preprocessor
+/*One set of classes, used to create input and output capabilities, is defined in the
+iostream header file */
+#include<iostream>
 using namespace std;
+// The main() function identifies the starting point of a C++ program
   int main(){
     cout("learnig c++ as a WIZARD! \n haha!");
     return 0;

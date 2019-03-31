95d8da7066bf6989b02e8b738436cd5ea19a8f0c
PouriaBakhshi <39597756+pouriabakhshi@users.noreply.github.com>
2019-03-31 20:20:08 +0430

    Add files via upload
    new code c++

---
+++ Cpp/areaPerimeter.cpp
@@ -0,0 +1,27 @@
+
+//**************************************************************
+// Given the length and width of a rectangle, this C++ program
+// computes and outputs the perimeter and area of the rectangle.
+//**************************************************************
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    double length;
+    double width;
+    double area;
+    double perimeter;
+
+    cout << "Enter length and width: ";
+    cin >> length >> width;
+    cout << endl;
+
+    perimeter = 2 * (length + width);
+    area = length *  width;
+
+    cout << "Length = " << length << ", width = " << width
+         << ", perimeter = " << perimeter << ", area = " << area << endl;
+system("pause");
+return 0;
+}
